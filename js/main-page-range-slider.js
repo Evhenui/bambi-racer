0eea2ec4ef44261a321d1d5bf1b00227213e845b
buryk20 <burykseriy@gmail.com>
2022-07-11 23:02:26 +0300

    slider-start-adaptiv

--- js/main-page-range-slider.js
+++ js/main-page-range-slider.js
@@ -3,9 +3,25 @@ export let mainPageRangeFun = function() {
     if(mainPageRange != null) {
         const mainPageRangeArr = mainPageRange.querySelectorAll('.js-main-page-range');
         const mainPageRangeCircle = mainPageRange.querySelectorAll('.js-main-page-range-circle-wrp');
-        console.log(mainPageRangeCircle);
         const mainPageRangeCircleRed = mainPageRange.querySelectorAll('.js-main-page-range-circle');
         const mainPageRangeImgArr = mainPageRange.querySelectorAll('.js-main-page-range-img');
+        const widthSliderTotal = mainPageRange.querySelector('.js-width-slid');
+        let width;
+        let count = 0;
+        // document.addEventListener("DOMContentLoaded", function(){
+        //     window.setTimeout(function(){
+        //         // do {
+        //         //     setTimeout(function(){
+        //         //         count++
+        //         //     // if(count > (mainPageRangeImgArr.length -1)) {
+        //         //     //     count = 0;
+        //         //     // }
+        //         //     roolSlider();
+        //         //     }, 300)
+                
+        //         // } while(count < 20)
+        //     }, 300)
+        // });
         mainPageRangeArr.forEach((el, index) => {
             el.addEventListener('click', function() {
                 delActivRange(mainPageRangeCircle);
@@ -15,13 +31,41 @@ export let mainPageRangeFun = function() {
                 mainPageRangeImgArr[index].classList.add('active')
                 el.querySelector('.main-page-range__circle').classList.add('active');
                 el.querySelector('.js-main-page-range-circle').classList.add('active');
+                if(window.innerWidth < 640) {
+                    count = index;
+                    roolSlider();
+                }
             })
         });
         function delActivRange(param) {
-            console.log(param);
             param.forEach((el) => {
                 el.classList.remove('active')
             })
         }
+
+        // моб логика
+        
+        console.log(width);
+        
+        function init() {
+            width = mainPageRange.querySelector('.js-width-slid-wrp').offsetWidth;
+            roolSlider();
+            if(window.innerWidth < 640) {
+                
+                widthSliderTotal.style.width = width * mainPageRangeImgArr.length + 'px'; 
+            }
+            mainPageRangeImgArr.forEach(item => {
+                console.log(width )
+                item.style.width = width + 'px';
+            })
+        }
+        window.addEventListener('resize', init);
+        init()
+
+        function roolSlider() {
+            console.log(count);
+            widthSliderTotal.style.transform = 'translate(-'+count*width+'px)';
+        }
+        
     }
 }
