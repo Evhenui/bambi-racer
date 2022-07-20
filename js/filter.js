64eee5931cec39e45c6d7ade94b7d0f12c9c9130
buryk20 <burykseriy@gmail.com>
2022-07-20 12:03:05 +0300

    product-filters

--- js/filter.js
+++ js/filter.js
@@ -7,22 +7,38 @@ export let filters = function () {
         const filterDefault = filterWrp.querySelector('.js-filter-default');
         const filterActive = filterWrp.querySelector('.js-filter-active');
         filterBntOpen.addEventListener('click', function (event) {
-            filterWrpContent.classList.toggle('active')
+            filterWrpContent.classList.toggle('active');
+            if (filterWrp.querySelector('.product-catalog-wrp-prod').classList.contains('active')) {
+                setTimeout(function() {
+                    filterWrp.querySelector('.product-catalog-wrp-prod').classList.remove('active');
+                }, 200)
+            } else {
+                filterWrp.querySelector('.product-catalog-wrp-prod').classList.add('active');
+            }
+            
+            filterWrp.querySelector('.product-catalog-filters-static').classList.toggle('active');
+            if(filterDefault.classList.contains('active')) {
+                filterDefault.classList.remove('active');
+                filterActive.classList.remove('active');
+            } else {
+                filterDefault.classList.add('active');
+                filterActive.classList.add('active');
+            }
         });
         filterBtnItem.forEach((el) => {
             el.addEventListener('click', function() {
                el.closest('.product-catalog-filters__container').querySelector('.js-content-item').classList.toggle('active');
-               el.classList.toggle('active')
+               el.classList.toggle('active');
             })
             
         });
-        filterDefault.addEventListener('click', function() {
-            this.classList.add('active');
-            filterActive.classList.add('active');
-        });
-        filterActive.addEventListener('click', function() {
-            this.classList.remove('active');
-            filterDefault.classList.remove('active')
-        })
+        // filterDefault.addEventListener('click', function() {
+        //     filterDefault.classList.add('active');
+        //     filterActive.classList.add('active');
+        // });
+        // filterActive.addEventListener('click', function() {
+        //     filterActive.classList.remove('active');
+        //     filterDefault.classList.remove('active')
+        // })
     }
 }
