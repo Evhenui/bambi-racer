002d3e7d5d8d320fee3b33f5531ce36c80772af4
Evhenui <73481852+Evhenui@users.noreply.github.com>
2022-08-19 16:19:23 +0300

    finish adaptive card-product-header-info

--- js/card-product.js
+++ js/card-product.js
@@ -57,7 +57,7 @@ export let cardProduct = function () {
     });
     //------hidden header----
     window.addEventListener("scroll", function () {
-      if (window.scrollY > 300 && window.innerWidth > 960) {
+      if (window.scrollY > 300) {
         headerWrapper.classList.add("active");
         buttonUp.classList.add("active");
       } else {
