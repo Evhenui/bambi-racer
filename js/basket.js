94079882aa740e65cd189d1e85788312f35521a2
Evhenui <73481852+Evhenui@users.noreply.github.com>
2022-08-24 01:11:52 +0300

    selects section

--- js/basket.js
+++ js/basket.js
@@ -1,3 +1,25 @@
 export let basket = function () {
+    const basket = document.querySelector('[data-basket-section]');
+  
+    if(basket !== null) {
+      const dropdownRegion = basket.querySelector('[data-dropdown-region]'),
+            dropdownRegionItems = basket.querySelectorAll('[data-dropdown-region-item]'),
+            dropdownSelectRegion = basket.querySelector('[data-select-region]');
 
+            dropdownSelectRegion.addEventListener('focus', function() {
+                dropdownRegion.classList.add('active');
+            })
+
+            dropdownSelectRegion.addEventListener('blur', function() {
+                dropdownRegion.classList.remove('active');
+            })
+
+            dropdownRegionItems.forEach((item) => {
+                item.addEventListener('click', function() {
+                    dropdownSelectRegion.value = this.innerHTML;
+                })
+            })
+    }
+
+    
 }
