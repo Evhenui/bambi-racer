2cd682c058557ab23a64790e6a5c9bc58fdb9193
Evhenui <73481852+Evhenui@users.noreply.github.com>
2022-08-24 14:34:13 +0300

    work with inputs

--- js/basket.js
+++ js/basket.js
@@ -2,24 +2,36 @@ export let basket = function () {
     const basket = document.querySelector('[data-basket-section]');
   
     if(basket !== null) {
-      const dropdownRegion = basket.querySelector('[data-dropdown-region]'),
-            dropdownRegionItems = basket.querySelectorAll('[data-dropdown-region-item]'),
-            dropdownSelectRegion = basket.querySelector('[data-select-region]');
+        
+        const dropdownRegion = basket.querySelector('[data-dropdown-region]'),
+              dropdownRegionSelectItems = basket.querySelectorAll('[data-dropdown-region-item]'),
+              dropdownRegionSelected = basket.querySelector('[data-dropdown-region-selected]'),
+              dropdownRegionLabel = basket.querySelector('[data-dropdown-region-label]');
 
-            dropdownSelectRegion.addEventListener('focus', function() {
-                dropdownRegion.classList.add('active');
-            })
-
-            dropdownSelectRegion.addEventListener('blur', function() {
-                dropdownRegion.classList.remove('active');
-            })
-
-            dropdownRegionItems.forEach((item) => {
-                item.addEventListener('click', function() {
-                    dropdownSelectRegion.value = this.innerHTML;
+              dropdownRegionSelectItems.forEach((item)=> {
+                item.addEventListener('click', ()=> {
+                    dropdownRegionSelected.innerText = item.innerText;
                 })
             })
-    }
+        
+            dropdownRegion.addEventListener('click', function() {
+                this.classList.toggle('active');
+                if(dropdownRegionSelected.innerText === '') {
+                    dropdownRegionLabel.classList.toggle('active');
+                } 
+                //close
+                basket.addEventListener('click',(e)=>{
+                    const click = e.composedPath().includes(this);
+                    if(!click) {
+                        this.classList.remove('active');
+                        if(dropdownRegionSelected.innerText === '') {
+                            dropdownRegionLabel.classList.remove('active');
+                        }   
+                    }
+                  })
+            })
 
+
+    }
     
 }
